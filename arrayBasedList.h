6f5746bbc9ebe52bab68a4f8e8fe32dfe00e5b36
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2023-04-08 14:34:09 +0200

    Adding Class Array Based List

--- arrayBasedList.h
+++ arrayBasedList.h
@@ -4,6 +4,106 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+class ArrayList {
+private:
+    T* data;
+    int capacity;
+    int size;
+
+public:
+    ArrayList(int capacity) : capacity(capacity), size(0) {
+        data = new T[capacity];
+    }
+
+    ~ArrayList() {
+        delete[] data;
+    }
+
+    void insert(T element) {
+        if (isFull()) {
+            throw overflow_error("List is full");
+        }
+        data[size] = element;
+        size++;
+    }
+
+    void insertAt(T element, int index) {
+        if (index < 0 || index > size) {
+            throw out_of_range("Invalid index");
+        }
+        if (isFull()) {
+            throw overflow_error("List is full");
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i-1];
+        }
+        data[index] = element;
+        size++;
+    }
+
+    T retrieveAt(int index) {
+        if (index < 0 || index >= size) {
+            throw out_of_range("Invalid index");
+        }
+        return data[index];
+    }
+
+    void removeAt(int index) {
+        if (index < 0 || index >= size) {
+            throw out_of_range("Invalid index");
+        }
+        for (int i = index; i < size-1; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+    }
+
+    void replaceAt(T newElement, int index) {
+        if (index < 0 || index >= size) {
+            throw out_of_range("Invalid index");
+        }
+        data[index] = newElement;
+    }
+
+    bool isItemAtEqual(T element, int index) {
+        if (index < 0 || index >= size) {
+            throw out_of_range("Invalid index");
+        }
+        return data[index] == element;
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+
+    int listSize() {
+        return size;
+    }
+
+    int maxListSize() {
+        return capacity;
+    }
+
+    void clear() {
+        size = 0;
+    }
+
+    void print() {
+        cout << "[";
+        for (int i = 0; i < size; i++) {
+            std::cout << data[i];
+            if (i < size-1) {
+                std::cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+    }
+};
 
 
 #endif //ASSIGNMENT2_DATA_STRUCTURE_ARRAYBASEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include "arrayBasedList.h"
 
-using namespace std;
-
-int main(){
-
-return 0;
+/*
+int main() {
+    ArrayList<int> list(5);
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.print();   // Output: [1, 2, 3]
+    list.insertAt(4, 1);
+    list.print();   // Output: [1, 4, 2, 3]
+    cout << list.retrieveAt(2) << endl;  // Output: 2
+    list.removeAt(0);
+    list.print();   // Output: [4, 2, 3]
+    list.replaceAt(5, 1);
+    list.print();   // Output: [4, 5, 3]
+    cout << boolalpha << list.isItemAtEqual(5, 1) << endl;  // Output: true
 }
+*/
