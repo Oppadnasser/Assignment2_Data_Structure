e0b02f55e04b331d01d9c3f07fdd81198202d750
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2023-04-08 15:58:48 +0200

    Adding Class Circular Linked List

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -5,5 +5,237 @@
 using namespace std;
 
 
+template <typename T>
+class CircularLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T element): data(element), next(nullptr) {}
+    };
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    CircularLinkedList(): head(nullptr), tail(nullptr), size(0) {}
+
+    void insertAtHead(T element) {
+        Node* newNode = new Node(element);
+        if(head == nullptr) {
+            head = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+            tail->next = head;
+        }
+        size++;
+    }
+
+    void insertAtEnd(T element) {
+        Node* newNode = new Node(element);
+        if(head == nullptr) {
+            head = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+        size++;
+    }
+
+    void insertAt(T element, int index) {
+        if(index < 0 || index > size) {
+            cout << "Invalid index!" << endl;
+            return;
+        }
+        if(index == 0) {
+            insertAtHead(element);
+            return;
+        }
+        if(index == size) {
+            insertAtEnd(element);
+            return;
+        }
+        Node* newNode = new Node(element);
+        Node* temp = head;
+        for(int i = 0; i < index-1; i++) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        size++;
+    }
+
+    void removeAtHead() {
+        if(head == nullptr) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        if(size == 1) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+            delete temp;
+        }
+        size--;
+    }
+
+    void removeAtEnd() {
+        if(head == nullptr) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        if(size == 1) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            Node* temp = head;
+            while(temp->next != tail) {
+                temp = temp->next;
+            }
+            temp->next = head;
+            delete tail;
+            tail = temp;
+        }
+        size--;
+    }
+
+    void removeAt(int index) {
+        if(index < 0 || index >= size) {
+            cout << "Invalid index!" << endl;
+            return;
+        }
+        if(index == 0) {
+            removeAtHead();
+            return;
+        }
+        if(index == size-1) {
+            removeAtEnd();
+            return;
+        }
+        Node* temp = head;
+        for(int i = 0; i < index-1; i++) {
+            temp = temp->next;
+        }
+        Node* toDelete = temp->next;
+        temp->next = toDelete->next;
+        delete toDelete;
+        size--;
+    }
+
+
+    T retrieveAt(int index) {
+        if(index < 0 || index >= size) {
+            cout << "Invalid index!" << endl;
+            return T();
+        }
+        Node* temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    void replaceAt(T newElement, int index) {
+        if(index < 0 || index >= size) {
+            cout << "Invalid index!" << endl;
+            return;
+        }
+        Node* temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        temp->data = newElement;
+    }
+
+    bool isExist(T element) {
+        if(head == nullptr) {
+            return false;
+        }
+        Node* temp = head;
+        do {
+            if(temp->data == element) {
+                return true;
+            }
+            temp = temp->next;
+        } while(temp != head);
+        return false;
+    }
+
+    bool isItemAtEqual(T element, int index) {
+        if(index < 0 || index >= size) {
+            cout << "Invalid index!" << endl;
+            return false;
+        }
+        Node* temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        return temp->data == element;
+    }
+
+    void swap(int firstItemIdx, int secondItemIdx) {
+        if(firstItemIdx < 0 || firstItemIdx >= size || secondItemIdx < 0 || secondItemIdx >= size || firstItemIdx == secondItemIdx) {
+            cout << "Invalid indices!" << endl;
+            return;
+        }
+        Node* firstItem = head;
+        for(int i = 0; i < firstItemIdx; i++) {
+            firstItem = firstItem->next;
+        }
+        Node* secondItem = head;
+        for(int i = 0; i < secondItemIdx; i++) {
+            secondItem = secondItem->next;
+        }
+        Node* temp = firstItem->next;
+        firstItem->next = secondItem->next;
+        secondItem->next = temp;
+        temp = firstItem->next->next;
+        firstItem->next->next = secondItem->next->next;
+        secondItem->next->next = temp;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+
+    int circularLinkedListSize() {
+        return size;
+    }
+
+    void clear() {
+        while(!isEmpty()) {
+            removeAtEnd();
+        }
+    }
+
+    void print() {
+        if(head == nullptr) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        Node* temp = head;
+        do {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while(temp != head);
+        cout << endl;
+    }
+};
+
 
 #endif //ASSIGNMENT2_DATA_STRUCTURE_CIRCULARLINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "arrayBasedList.h"
+#include "CircularLinkedList.h"
 
 /*
 int main() {
@@ -15,5 +16,62 @@ int main() {
     list.replaceAt(5, 1);
     list.print();   // Output: [4, 5, 3]
     cout << boolalpha << list.isItemAtEqual(5, 1) << endl;  // Output: true
+}
+*/
+
+/*
+int main() {
+    // create a new circular linked list
+    CircularLinkedList<int> cll;
+
+    // insert elements at head and end
+    cll.insertAtHead(3); // 3
+    cll.insertAtHead(2); // 2 3
+    cll.insertAtHead(1); // 1 2 3
+    cll.insertAtEnd(4); // 1 2 3 4
+    cll.insertAtEnd(5); // 1 2 3 4 5
+
+    // insert an element at index 2
+    cll.insertAt(10, 2); // 1 2 10 3 4 5
+
+    // remove elements from head and end
+    cll.removeAtHead(); // 2 10 3 4 5
+    cll.removeAtEnd(); // 2 10 3 4
+
+    // remove an element at index 2
+    cll.removeAt(2); // 2 10 4
+
+    // print the list
+    cout << "List contents: ";
+    cll.print(); // 2 10 4
+
+    // get the element at index 1
+    cout << "Element at index 1: " << cll.retrieveAt(1) << endl; // 10
+
+    // replace an element at index 0
+    cll.replaceAt(100, 0); // 100 10 4
+
+    // check if an element exists in the list
+    cout << "Element 4 exists in list: " << boolalpha << cll.isExist(4) << endl; // true
+
+    // check if an element at index 1 is equal to a value
+    cout << "Element at index 1 is equal to 10: " << cll.isItemAtEqual(10, 1) << endl; // true
+
+    // Add new element in head to check the swap
+    cll.insertAtHead(1); // 1 100 10 4
+
+    // swap two nodes
+    cll.swap(1, 3); // 1 4 10 100
+
+    // print the list after swapping nodes
+    cout << "List contents after swapping nodes: ";
+    cll.print(); // 1 4 10 100
+
+    // clear the list
+    cll.clear(); // 0
+
+    // check if the list is empty
+    cout << "List is empty: " << boolalpha << cll.isEmpty() << endl;
+
 }
 */
